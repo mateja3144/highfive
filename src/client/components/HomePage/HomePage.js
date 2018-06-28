202b6711bb9d0fe12f1c8933209499c041caf12a
Adam Matejczyk <mateja3144@gmail.com>
2018-06-28 21:34:26 +0200

    Dodanie consta events który zostanie wykorzystany w wyświetlaniu wydarzeń. Jeszcze nie wiem jak je wykorzystać.  Treść narazie poglądowa

--- src/client/components/HomePage/HomePage.js
+++ src/client/components/HomePage/HomePage.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import style from './style.css';
+
+const events = [
+    {
+        date: '28 X 2017',
+        place: 'Gruba Blues',
+        city: 'Chorzów'
+    },
+    {
+        date: '12 XII 2017',
+        place: 'Dni Piekar Śląskich',
+        city: 'Piekary Śląskie'
+    },
+    {
+        date: '03 III 2017',
+        place: 'Festyn Sportowy',
+        city: 'Park Murckowski'
+    }
+];
 
 export const HomePage = () => (
-    <div>Strona główna</div>
+    <section className={style.section}>
+        <h2 className={style.title}>Gdzie gramy ?</h2>
+        <div className={style.events}>
+            28 X 2017 ---- Gruba Blues ---- Chorzów<br/>
+            12 X 2017 ---- Dni Piekary Śląskich ---- Piekary Śląskie<br/>
+            28 X 2017 ---- Festyn Sportowy ---- Park Murckowski<br/>
+        </div>
+    </section>
 );
 
 HomePage.propTypes = {
