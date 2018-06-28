0d82703c49c6e84300236f0ab03bd284ff26ecec
Adam Matejczyk <mateja3144@gmail.com>
2018-06-28 20:21:20 +0200

    opracanie menu, loga, treści

--- src/client/components/App/App.js
+++ src/client/components/App/App.js
@@ -10,20 +10,25 @@ import TextsPage from '../TextsPage';
 import PressPackPage from '../PressPackPage';
 import ContactPage from '../ContactPage';
 import Footer from '../Footer';
+import style from './style.css';
 
 export const App = ({ store }) => (
     <Provider store={store}>
         <BrowserRouter basename={'#' + window.location.pathname}>
             <div>
                 <Header />
-                <Switch>
-                    <Route path='/o_nas' component={AboutPage} />
-                    <Route path='/galeria' component={GalleryPage} />
-                    <Route path='/teksty' component={TextsPage} />
-                    <Route path='/press-pack' component={PressPackPage} />
-                    <Route path='/kontakt' component={ContactPage} />
-                    <Route path='/' component={HomePage} />
-                </Switch>
+                <main className={style.main}>
+                    <div className={style.container}>
+                        <Switch>
+                            <Route path='/o_nas' component={AboutPage} />
+                            <Route path='/galeria' component={GalleryPage} />
+                            <Route path='/teksty' component={TextsPage} />
+                            <Route path='/press-pack' component={PressPackPage} />
+                            <Route path='/kontakt' component={ContactPage} />
+                            <Route path='/' component={HomePage} />
+                        </Switch>
+                    </div>
+                </main>
                 <Footer />
             </div>
         </BrowserRouter>
